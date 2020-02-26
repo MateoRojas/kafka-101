a132aa65a6512924fbf05fd058c3be5351df21e3
Mateo Rojas <mateo.rojas@thoughtworks.com>
2020-02-25 20:58:51 -0500

    Adds logger and fixes consumer and producer

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -3,12 +3,16 @@ import java.util.List;
 import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class Consumer {
 
     public static void main(String[] args) {
         final var properties = new Properties();
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "some-id");
         final var consumer = new KafkaConsumer<String, String>(properties);
         consumer.subscribe(List.of("some-topic"));
         while (true)  {
